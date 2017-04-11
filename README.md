ea66066fd628fd9f3986bdbda5b0baf92c22a438
Tanmoy Biswas <bravotanmoy@gmail.com>
2017-04-12 03:52:29 +0600

    1st commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## AJAX Infinite Scrolling
+Infinite Scrolling one by obe as like facebook home page and blog or post listing page.
+My Resume || Protfolio website [Tanmoy Biswas](http://tanmoyb.com/)
---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+<!-- Meta For character encoding -->
+<meta charset="utf-8">
+<!-- Meta For Chrome Frame -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<!--  Meta For Description -->
+<meta name="description" content="">
+<!--  Meta For Responsive -->
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<title>Infinite Scrolling</title>
+
+<!--Simple Files-->
+<link rel="stylesheet" href="css/main.css"/>
+</head>
+<body>
+
+<div id="blog-posts">
+</div>
+
+<div id="spinner">
+  <img src="spinner.gif" width="50" height="50" />
+</div>
+
+<div id="load-more-container">
+  <button id="load-more" data-page="0">Load more</button>
+</div>
+
+
+
+
+
+
+
+
+<!-- Simple JavaScript Files -->
+<script src="js/simple.js"></script>
+</body>
+</html>
